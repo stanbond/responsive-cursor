88a5998fab1e4305d584083dd6a57ac03f8017ec
StanBond <stan.bond@ymail.com>
2019-10-18 14:35:18 -0600

    colors and respawn logic

--- app.js
+++ app.js
@@ -5,19 +5,52 @@ const canvas = canv.getContext('2d');
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 
+var maxBallRadius = 40;
+var minBallRadius = 3;
+var maxSpeed = 3;
+var balls = 1000;
+
+var mouse = {
+  x: undefined,
+  y: undefined
+};
+
+var colorArray = [
+  '#F5F5F5',
+  '#9E9E9E',
+  '#F99D1C',
+  '#1A4D7F'
+];
+
+window.addEventListener('mousemove', function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener('resize', function() {
+  canv.width = window.innerWidth;
+  canv.height = window.innerHeight;
+
+  init();
+});
+
 function Ball(x, y, dx, dy, radius){
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minBallRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function() {
     canvas.beginPath();
     canvas.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    canvas.strokeStyle = 'blue';
-    canvas.stroke();
-  }
+    // canvas.strokeStyle = 'lightgrey';
+    canvas.fillStyle = this.color;
+    // canvas.stroke();
+    canvas.fill();
+  };
 
   this.update = function() {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
@@ -31,6 +64,13 @@ function Ball(x, y, dx, dy, radius){
     this.x += this.dx
     this.y += this.dy
 
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+      if ( this.radius < maxBallRadius) {
+      this.radius += 1; }
+    } else if (this.radius > this.minBallRadius) {
+      this.radius -= 1;
+    }
+
     this.draw();
   }
 }
@@ -38,14 +78,19 @@ function Ball(x, y, dx, dy, radius){
 // var ball = new Ball(200, 200, 3, 3, 30)
 var ballArray = [];
 
-for (var i = 0; i < 100; i++) {
-  var radius = 30;
-  var x = Math.random() * (innerWidth - radius * 2) + radius;
-  var y = Math.random() * (innerHeight - radius * 2) + radius;
-  var dx = (Math.random() - 0.5) * 10;
-  var dy = (Math.random() - 0.5) * 10;
-  ballArray.push(new Ball(x,y,dx,dy,radius))
+function init() {
+  ballArray = [];
+
+  for (var i = 0; i < balls; i++) {
+    var radius = Math.random() * 3 + minBallRadius;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * maxSpeed;
+    var dy = (Math.random() - 0.5) * maxSpeed;
+    ballArray.push(new Ball(x, y, dx, dy, radius));
+  }
 }
+
 function animate() {
   requestAnimationFrame(animate);
   canvas.clearRect(0,0, innerWidth, innerHeight);
@@ -56,5 +101,6 @@ function animate() {
     ballArray[i].update();
   }
 }
-
+// animate();
+init();
 animate();
